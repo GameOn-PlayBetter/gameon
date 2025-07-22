026dba8371959b19e68dfa08c2fe66be8ef48107
Mandy Lopez <gameon.playbetter@gmail.com>
2025-07-22 13:42:28 -0500

    Initial commit

---
+++ src/ui/components/PieChart.tsx
@@ -0,0 +1,37 @@
+"use client";
+/*
+ * Documentation:
+ * Pie Chart — https://app.subframe.com/library?component=Pie+Chart_0654ccc7-054c-4f3a-8e9a-b7c81dd3963c
+ */
+
+import React from "react";
+import * as SubframeUtils from "../utils";
+import * as SubframeCore from "@subframe/core";
+
+interface PieChartRootProps
+  extends React.ComponentProps<typeof SubframeCore.PieChart> {
+  className?: string;
+}
+
+const PieChartRoot = React.forwardRef<HTMLElement, PieChartRootProps>(
+  function PieChartRoot({ className, ...otherProps }: PieChartRootProps, ref) {
+    return (
+      <SubframeCore.PieChart
+        className={SubframeUtils.twClassNames("h-52 w-52", className)}
+        ref={ref as any}
+        colors={[
+          "#5064dc",
+          "#005064",
+          "#783cf0",
+          "#007896",
+          "#a343ff",
+          "#00a0c8",
+        ]}
+        dark={true}
+        {...otherProps}
+      />
+    );
+  }
+);
+
+export const PieChart = PieChartRoot;
