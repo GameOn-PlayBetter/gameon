c2329271885b6aa49ea0f47dc61abb9216d80c77
Mandy Lopez <gameon.playbetter@gmail.com>
2025-08-05 18:25:56 -0500

    failed attempt to update favicon but updated social favicons hopefully

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,19 +13,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        {/* eslint-disable-next-line */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+<head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link
+    rel="preconnect"
+    href="https://fonts.gstatic.com"
+    crossOrigin="anonymous"
+  />
+  {/* eslint-disable-next-line */}
+  <link
+    href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet"
+  />
+
+  {/* ✅ Favicon */}
+  <link rel="icon" href="/favicon.ico" sizes="any" />
+  <link rel="apple-touch-icon" href="/skillery_social_512.png" />
+
+  {/* ✅ Social previews */}
+  <meta property="og:image" content="https://skillery.co/skillery_social_512.png" />
+  <meta property="twitter:image" content="https://skillery.co/skillery_social_512.png" />
+  <meta property="og:title" content="Skillery" />
+  <meta property="og:description" content="Unlock your potential." />
+</head>
 
       <body>{children}</body>
     </html>
