bfaa5d8f5b33ac1273924914575026f489b94f65
Mandy Lopez <gameon.playbetter@gmail.com>
2025-08-05 19:48:40 -0500

    add vercel site tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'        // only if you have global styles
+import '../src/app/globals.css'
 import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }) {
